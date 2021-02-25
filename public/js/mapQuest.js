732a1072d0a55eb99c09673252b663c0712e4cf1
Joey Hoang <j4hoang@ucsd.edu>
2021-02-25 10:18:42 -0800

    thirdparty API lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'OhGZigmA4WfNGAgTNjbDoLHXv0ZjeHpw';
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [37.7749, -122.4194],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	  });
+
+	//Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
+	L.marker([32.87897128257093, -117.23575787568393]).addTo(map);
 }
